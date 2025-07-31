9135e0f61cebcee726ff95c4d44bc26ffb763174
Adrian-Koh <adriankoh216@yahoo.com>
2025-07-31 13:31:20 +0800

    Update app to use Express

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
-const http = require("node:http");
-const fs = require("node:fs");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const VALID_URLS = ["/", "/about", "/contact-me"];
-const DEFAULT_PORT = 8080;
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/about", (req, res) =>
+  res.sendFile(path.join(__dirname, "about.html"))
+);
+app.get("/contact-me", (req, res) =>
+  res.sendFile(path.join(__dirname, "contact-me.html"))
+);
+app.use(express.static(path.join(__dirname)));
 
-// Create a local server to receive data from
-const server = http.createServer((req, res) => {
-  let filePath;
-  if (VALID_URLS.includes(req.url)) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    switch (req.url) {
-      case "/":
-        filePath = "index.html";
-        break;
-      case "/about":
-        filePath = "about.html";
-        break;
-      case "/contact-me":
-        filePath = "contact-me.html";
-        break;
-    }
-  } else if (req.url === "/styles.css") {
-    const fileStream = fs.createReadStream("styles.css", "UTF-8");
-    res.writeHead(200, { "Content-Type": "text/css" });
-    fileStream.pipe(res);
-    return;
-  } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    filePath = "404.html";
-  }
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    res.write(data);
-  });
-});
+app.use((req, res) =>
+  res.status(404).sendFile(path.join(__dirname, "404.html"))
+);
 
-server.listen(DEFAULT_PORT, () => {
-  console.log(`Server listening on port ${DEFAULT_PORT}.`);
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
 });
